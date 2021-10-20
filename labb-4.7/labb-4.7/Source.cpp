83ae0fede03dea3a7e4253ad5aab0ac7f25c2bd1
Yuliia <ylua.vasylyk.04@gmail.com>
2021-10-20 13:41:02 +0300

    hello

---
+++ labb-4.7/labb-4.7/Source.cpp
@@ -0,0 +1,58 @@
+// Lab_04_7.cpp
+// <Василик Юлія>
+// Лабораторна робота №4.7
+// Обчислення суми ряду Тейлора за допомогою ітераційних циклів та рекурентних співвідношень
+// Варіант 2
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+    double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+    int n = 0;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(10) << "exp(x)" << " |"
+        << setw(10) << "S" << " |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+    x = xp;
+
+    while (x <= xk)
+    {
+        n = 0;
+        a = 1;
+        S = a;
+
+        do
+        {
+            n++;
+            R = -(1. * x / n);
+            a *= R;
+            S += a;
+        } while (abs(a) >= eps);
+
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << exp(-x) << " |"
+            << setw(10) << setprecision(5) << S << " |"
+            << setw(5) << n << " |"
+            << endl;
+        x += dx;
+    }
+
+    cout << "-------------------------------------------------" << endl;
+    return 0;
+}
